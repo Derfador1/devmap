a023f05a55ac16b14aea81d5e0cf44f7180fff14
Derfador1 <Derfador1@gmail.com>
2016-02-23 11:45:19 -0500

    added in a check for if there is a value or not after p

--- devmap.c
+++ devmap.c
@@ -1,4 +1,6 @@
 #include <stdio.h>
+#include <math.h>
+#include <ctype.h>
 #include "decoder.h"
 
 #define PI 3.14159265
@@ -8,6 +10,8 @@ int main(int argc, char *argv[])
 {
 	int file_count = 1;
 	int descrip = 0;
+	double battery_life;
+	char *ptr;
 
 	if(argc == 1) {
 		printf("Please retry with a valid file to open.\n");
@@ -16,6 +20,25 @@ int main(int argc, char *argv[])
 	else if (argc >= 2) {
 		if(strncmp(argv[file_count], "-p", 10) == 0) {
 			file_count++;
+			printf("Num: %d\n", file_count);
+			//battery_life = strtol(argv[file_count], &ptr, 10);
+			char *tester = argv[file_count];
+			for(unsigned int d = 0; d < strlen(argv[2]); ++d){
+				if(isdigit(tester[d]) != 0){
+					//
+				}
+				else{
+					//
+				}
+			}
+
+			battery_life = strtol(argv[file_count], &ptr, 10);
+			if(battery_life <= 0){
+
+			}
+			else {
+				file_count++;
+			}
 		}
 
 		descrip = open(argv[file_count], O_RDONLY); //gives an integer if open works successfully 
@@ -35,4 +58,21 @@ int main(int argc, char *argv[])
 	}
 }
 
+//this code was taken from http://stackoverflow.com/questions/26446308/issues-with-a-result-from-calculating-latitude-longitude-from-haversine-formula
+double haversine(double lat1, double lat2, double lon1, double lon2) 
+{
+	//conversion to radians
+	lat1 *= PI/180;
+	lat2 *= PI/180;
+	lon1 *= PI/180;
+	lon2 *= PI/180;
 
+	//getting the difference for lat and lon
+	double dlat = (lat2 - lat1);
+	double dlon = (lon2 - lon1);
+
+	//math
+	double a = pow(sin(dlat/2), 2) + cos(lat1) * cos(lat2) * pow(sin(dlon/2), 2);
+	double b = 2 * atan2(sqrt(a), sqrt(1-a));
+	return EARTH_RAD * b;
+}
