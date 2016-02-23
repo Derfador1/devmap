a2cd9a50c4efdf5b7b64518be3cb5c7d58f0077d
Derfador1 <Derfador1@gmail.com>
2016-02-23 10:02:31 -0500

    added a check to see if a -p was there

--- devmap.c
+++ devmap.c
@@ -1,15 +1,23 @@
 #include <stdio.h>
 #include "decoder.h"
 
+#define PI 3.14159265
+#define EARTH_RAD 6356.752
+
 int main(int argc, char *argv[]) 
 {
 	int file_count = 1;
 	int descrip = 0;
+
 	if(argc == 1) {
 		printf("Please retry with a valid file to open.\n");
 		exit(1);
 	}
 	else if (argc >= 2) {
+		if(strncmp(argv[file_count], "-p", 10) == 0) {
+			file_count++;
+		}
+
 		descrip = open(argv[file_count], O_RDONLY); //gives an integer if open works successfully 
 		if (descrip == -1)
 		{
@@ -26,3 +34,5 @@ int main(int argc, char *argv[])
 		file_count++;
 	}
 }
+
+
