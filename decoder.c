c4123ad0afa55c989c73644470f3836fd9fa0a9f
Derfador1 <Derfador1@gmail.com>
2016-02-26 08:51:21 -0500

    removed all the dijkstra functions from this file

--- decoder.c
+++ decoder.c
@@ -1,12 +1,5 @@
-#include <stdbool.h>
-#include <stdio.h>
-#include <inttypes.h>
-
 #include "decoder.h"
-#include "llist.h"
-#include "hash.h"
-#include "heap.h"
-#include "queue.h"
+#include "dijkstra.h"
 #include "graph/graph.h"
 #include <math.h>
 
@@ -39,173 +32,6 @@ struct ipv4 *make_ip(void) //used to initialize meditrik structure when invoked
 	}
 
 	return ipv4;
-}
-
-struct pqueue_node {
-	const void *data;
-	int priority;
-};
-
-int pq_compare(const void *a, const void *b)
-{
-	if(!a) {
-		return -(intptr_t)b;
-	}
-	if(!b) {
-		return (intptr_t)a;
-	}
-
-	struct pqueue_node *pqa, *pqb;
-	pqa = (struct pqueue_node *)a;
-	pqb = (struct pqueue_node *)b;
-
-	return pqa->priority - pqb->priority;
-}
-
-struct pqueue_node *__make_node(const void *data, int priority)
-{
-	struct pqueue_node *pqn = malloc(sizeof(*pqn));
-	if(!pqn) {
-		return NULL;
-	}
-	pqn->data = data;
-	pqn->priority = priority;
-
-	return pqn;
-}
-
-struct visited_node {
-	int distance;
-	struct pqueue_node *priority;
-	const void *prev;
-};
-
-struct visited_node *__make_vnode(int distance,
-		struct pqueue_node *priority, const void *prev)
-{
-	struct visited_node *vis = malloc(sizeof(*vis));
-	if(!vis) {
-		return NULL;
-	}
-	vis->prev = prev;
-	vis->distance = distance;
-	vis->priority = priority;
-
-	return vis;
-}
-
-struct llist *dijkstra_path(const graph *g, const void *from, const void *to)
-{
-	heap *to_process = heap_create(pq_compare);
-	struct pqueue_node *start =__make_node(from, 0);
-	heap_add(to_process, start);
-
-	hash *visited = hash_create();
-	struct visited_node *first = __make_vnode(0, start, NULL);
-	hash_insert(visited, from, first);
-
-	while(!heap_is_empty(to_process)) {
-		struct pqueue_node *curr = heap_remove_min(to_process);
-
-		if(curr->data == to) {
-			free(curr);
-			goto FOUND;
-		}
-
-		struct llist *adjacencies = graph_adjacent_to(g, curr->data);
-		struct llist *check = adjacencies;
-		while(check) {
-
-			int dist = curr->priority +
-				graph_edge_weight(g, curr->data, check->data);
-
-			if(!hash_exists(visited, check->data)) {
-				struct pqueue_node *pq_to_add =__make_node(check->data, dist);
-
-				struct visited_node *next_node =
-					__make_vnode(dist, pq_to_add, curr->data);
-
-				hash_insert(visited, check->data, next_node);
-				heap_add(to_process, pq_to_add);
-			} else {
-				struct visited_node *found = hash_fetch(visited, check->data);
-
-				if(dist < found->distance) {
-					found->distance = dist;
-					found->prev = curr->data;
-					found->priority->priority = dist;
-					heap_rebalance(to_process);
-				}
-			}
-
-			check = check->next;
-		}
-		free(curr);
-		ll_disassemble(adjacencies);
-	}
-	heap_destroy(to_process);
-	hash_destroy(visited);
-
-	return NULL;
-
-FOUND:
-	heap_destroy(to_process);
-
-	struct llist *path = ll_create(to);
-	while(((struct visited_node *)hash_fetch(visited, path->data))->prev) {
-		ll_add(&path,
-				((struct visited_node *)hash_fetch(visited, path->data))->prev);
-	}
-
-	hash_destroy(visited);
-
-	return path;
-}
-
-struct llist *graph_path(const graph *g, const void *from, const void *to)
-{
-	hash *visited = hash_create();
-	queue *to_process = queue_create();
-
-	hash_insert(visited, from, NULL);
-	queue_enqueue(to_process, from);
-
-	while(!queue_is_empty(to_process)) {
-		void *curr = queue_dequeue(to_process);
-
-		struct llist *adjacencies = graph_adjacent_to(g, curr);
-		struct llist *check = adjacencies;
-		while(check) {
-			if(!hash_exists(visited, check->data)) {
-				hash_insert(visited, check->data, curr);
-				queue_enqueue(to_process, check->data);
-				if(check->data == to) {
-					ll_disassemble(adjacencies);
-					goto FOUND;
-				}
-			}
-
-			check = check->next;
-		}
-
-		ll_disassemble(adjacencies);
-	}
-
-	queue_disassemble(to_process);
-	hash_disassemble(visited);
-	return NULL;
-
-FOUND:
-	queue_disassemble(to_process);
-
-	struct llist *path = ll_create(to);
-	while(hash_fetch(visited, path->data)) {
-		ll_add(&path, hash_fetch(visited, path->data));
-	}
-
-	hash_disassemble(visited);
-
-	return path;
 }
 
 void ll_print(struct llist *test)
@@ -311,24 +137,19 @@ struct llist *extraction(char * argv[])
 		if(src_port != 57005 || dst_port != 57005) {
 			printf("This is a malformed packet\n");
 			*start = *start + *total_length - medi_header;
-			printf("Count:%d Start1:%d\n", count, *start);
-			free(stuff);
-			free(ver);
+			//printf("Count:%d Start1:%d\n", count, *start);
 			free(data); //the hell? WHY
-			continue;
+			goto END;
 		}
 
 		/*
-		if(*type_pt != 2) {
-			printf("um\n");
-			continue;
-		}
 		fprintf(stdout, "Version: %d\n", stuff->version);
 		fprintf(stdout, "Type: %d\n", stuff->type);
 		fprintf(stdout, "Source Device: %d\n", stuff->source_device_id);
 		fprintf(stdout, "Destination Device: %d\n", stuff->dest_device_id);
 		fprintf(stdout, "Sequence: %d\n", stuff->seq_id);
 		*/
+
 		data->battery_power = 0;
 		data->latitude = 0;
 		data->longitude = 0;
@@ -344,25 +165,23 @@ struct llist *extraction(char * argv[])
 			data->source_dev_id = stuff->source_device_id;
 
 			if(test) {
-				//printf("add\n");
 				ll_add(&test, data);
 			}
 			else {
-				//printf("create\n");
 				test = ll_create(data);				
 			}
 		}
 		else {
+			printf("There was not gps data to be found\n");
 			data->latitude = 0;
 			data->longitude = 0;
 			data->altitude = 0;
 		}
 
-		printf("Count:%d Start:%d\n", count, *start);
-
+		//printf("Count:%d Start:%d\n", count, *start);
+END:
 		free(stuff);
 		free(ver);
-
 	}
 
 	free(buf);
@@ -411,7 +230,6 @@ graph *ll_to_graph(graph *g, struct llist *l)
 			const struct device *tmp_l = l->data;
 			const struct device *tmp2 = tmp->data;
 			result = haversine(tmp_l->latitude, tmp2->latitude, tmp_l->longitude, tmp2->longitude, tmp_l->altitude, tmp2->altitude);
-			//printf("Result: %f\n", result);
 			if(result > 1.25 && result < 5.00) {
 				printf("Result: %f\n", result);
 				graph_add_edge(g, tmp_l, tmp2, result);
@@ -439,7 +257,7 @@ void print_item(const void *data, bool is_node)
 		printf("\n");
 	} 
 	else {
-		printf(u8" → %d", *(int *)data);
+		//printf(u8" → %d", *(int *)data);
 	}
 }
 
@@ -606,8 +424,6 @@ int status_decode(struct device *data, int *start, unsigned char *buf, int count
 	data->battery_power = (power.percent * 100);
 
 	printf("Battery_power %.02lf%%\n", data->battery_power);
-
-	//fprintf(stdout, "Battery power : %.2f%%\n", power.percent * 100);
 
 	unsigned int glucose_start = buf[*start]; 
 	glucose_start <<= 8;
