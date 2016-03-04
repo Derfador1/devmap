7c03fa3fb8d733eef8fb678afcc17a6ea8f68bc3
Derfador1 <Derfador1@gmail.com>
2016-03-03 19:41:13 -0500

    first use of header file for devmap

---
+++ devmap.h
@@ -0,0 +1,13 @@
+#ifndef DEVMAP_H
+	#define DEVMAP_H
+
+#include "decoder.h"
+
+bool is_vendor_recommended(graph *g, struct llist *l);
+void parse_args(int argc, char *argv[], int *tmp_file_count, double *battery_life);
+bool removing(graph *g, struct llist *l);
+double haversine(double lat1, double lat2, double lon1, double lon2, float alt1, float alt2);
+graph *ll_to_graph(graph *g, struct llist *l);
+struct llist *count(graph *g, struct llist *l);
+
+#endif
